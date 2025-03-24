d27b6fb5f21e3b071462d50af8796b02f5e17408
Weili <1571916135@qq.com>
2025-03-24 15:11:54 +0800

    some basic functionalities

--- Assets/VoxelEngine/VoxelManager/ChunkManager/ChunkManager.cs
+++ Assets/VoxelEngine/VoxelManager/ChunkManager/ChunkManager.cs
@@ -2,21 +2,46 @@ using UnityEngine;
 using Unity.Collections;
 using Unity.Mathematics;
 
-namespace VoxelEngine.VoxelManager.ChunkManager
+namespace VoxelEngine.VoxelManager
 {
     /// <summary>
     /// Chunk Manager stores mapping of chunk position to index.
     /// It stores dirty state of chunks.
+    /// It stores transformation data.
     /// </summary>
     public partial class ChunkManager
     {
+        public const int CHUNK_WIDTH = 32;
+
         NativeHashMap<int3, int> chunkMap;
+        int numChunkAdded;
+
         NativeHashSet<int3> dirtyChunks;
+        TransformManager transformManager;
 
         public ChunkManager(int maxNumChunk)
         {
+            numChunkAdded = 0;
             chunkMap = new NativeHashMap<int3, int>(maxNumChunk, Allocator.Persistent);
             dirtyChunks = new NativeHashSet<int3>(maxNumChunk, Allocator.Persistent);
+        }
+
+        public void AddChunk(int3 chunkPosition)
+        {
+            if (chunkMap.TryAdd(chunkPosition, numChunkAdded))
+            {
+                transformManager.AddChunk(chunkPosition);
+                SetChunkDirty(chunkPosition);
+                numChunkAdded++;
+            }
+        }
+
+        public void SetChunkDirty(int3 chunkPosition)
+        {
+            if (chunkMap.ContainsKey(chunkPosition))
+            {
+                dirtyChunks.Add(chunkPosition);
+            }
         }
     }
 }
--- Assets/VoxelEngine/VoxelManager/TransformManager/TransformManager.cs
+++ Assets/VoxelEngine/VoxelManager/TransformManager/TransformManager.cs
@@ -2,27 +2,43 @@ using UnityEngine;
 using Unity.Collections;
 using Unity.Mathematics;
 
-namespace VoxelEngine.VoxelManager.TransformManager
+namespace VoxelEngine.VoxelManager
 {
-    /// <summary>
-    /// Transform Manager stores localScale and position of a voxel space.
-    /// It stores bounding boxes of chunks.
+	/// <summary>
+	/// Transform Manager stores localScale and position of a voxel space.
+	/// It stores bounding boxes of chunks.
 	/// It stores frustum culling data of chunks.
-    /// </summary>
-    public partial class TransformManager
-    {
+	/// </summary>
+	public partial class TransformManager
+	{
 		float3 scale;
-		NativeArray<int3> chunkAdded;
-		NativeArray<Bounds> chunkBounds;
-		NativeHashMap<int3, int> chunkMap;
+		NativeList<int3> chunkAdded;
+		NativeList<Bounds> chunkBounds;
 
-		public TransformManager(int maxNumChunk, float3 _scale, NativeHashMap<int3, int> _chunkMap)
+		public TransformManager(int maxNumChunk, float3 _scale)
 		{
 			scale = _scale;
-			chunkMap = _chunkMap;
 
-			chunkAdded = new NativeArray<int3>(maxNumChunk, Allocator.Persistent);
-			chunkBounds = new NativeArray<Bounds>(maxNumChunk, Allocator.Persistent);
+			chunkAdded = new NativeList<int3>(maxNumChunk, Allocator.Persistent);
+			chunkBounds = new NativeList<Bounds>(maxNumChunk, Allocator.Persistent);
+		}
+
+		public void AddChunk(int3 chunkPosition)
+		{
+			chunkAdded.Add(chunkPosition);
+			AddChunkBounds(chunkPosition);
+		}
+
+		public void AddChunkBounds(int3 chunkPosition)
+		{
+			int3 actualPosition = chunkPosition * ChunkManager.CHUNK_WIDTH;
+			float3 chunkCenter = ((float3)actualPosition + ChunkManager.CHUNK_WIDTH * 0.5f) * scale;
+			float3 chunkScales = new float3(
+				ChunkManager.CHUNK_WIDTH,
+				ChunkManager.CHUNK_WIDTH,
+				ChunkManager.CHUNK_WIDTH
+			) * scale;
+			chunkBounds.Add(new Bounds(chunkCenter, chunkScales));
 		}
     }
 }
